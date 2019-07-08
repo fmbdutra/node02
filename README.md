c5d8eeed52004002458f85045a60edd716d31ca5
Fabiano M. Dutra <38920482+fmbdutra@users.noreply.github.com>
2019-07-08 10:29:50 -0300

    Arquivos do curso 1 necessários

---
+++ README.md
@@ -0,0 +1,8 @@
+# nodejs02
+Projeto do curso de Node.js do Alura
+<br/><br/>
+Curso: Node.js Parte 2: MVC, autenticação e autorização
+<br/>
+<br/>
+Cachoeirinha, RS - Brasil <br>
+Julho, 2019
---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,81 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
+module.exports = (app) => {
+    app.get('/', function(req, resp) {
+        resp.send(
+            `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+            `
+        );
+    });
+    
+    //CRUD---------------------
+
+    //Lista Livros
+    app.get('/livros', function(req, resp) {
+        const livroDao = new LivroDao(db)
+        livroDao.lista()
+                .then(livros => resp.marko(
+                    require('../views/livros/lista/lista.marko'), 
+                    {
+                        livros: livros
+                    }           
+                ))
+                .catch (erro => console.log(erro));      
+    });
+
+    app.get('/livros/form', function(req, resp){
+        resp.marko(require('../views/livros/form/form.marko'), {livro: {} });
+    });
+
+    //Adição de livros
+    app.post('/livros', function(req, resp) {
+        console.log(req.body); //Serão enviados para o corpo da requisição
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros')) 
+                .catch (erro => console.log(erro));
+    })
+
+    //Para edição de livros
+    app.put('/livros', function(req, resp) {
+        console.log(req.body); 
+        const livroDao = new LivroDao(db);
+        livroDao.atualiza(req.body)
+                .then(resp.redirect('/livros')) 
+                .catch (erro => console.log(erro));
+    })
+
+    //Remover livros
+    app.delete('/livros/:id', function(req, resp){
+        const id = req.params.id;
+        
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
+    })
+
+    //Buscar ID
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    });
+}
